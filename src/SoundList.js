e58a271d2b25393810ab60ae1731702ce9e360ef
endziu <koper.andrzej@gmail.com>
2016-08-15 12:43:59 +0200

    change <Soundlist /> to use arrow function syntax

--- src/SoundList.js
+++ src/SoundList.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class SoundList extends Component {
-  render () {
-    const makeListItem = (item) =>
-      <li key={item.id} className='listItem'>
-        <img className='listAvatar' src={item.artwork_url} alt='artwork' />
-        <div className='userInfo'>
-          <div id='user'>{item.userName}</div>
-          <div id='title'>{item.title}</div>
-        </div>
-      </li>
+const ListItem = (props) =>
+  <li key={props.id} className='listItem'>
+    <img className='listAvatar' src={props.artwork_url} alt='artwork' />
+    <div className='userInfo'>
+      <div id='user'>{props.userName}</div>
+      <div id='title'>{props.title}</div>
+    </div>
+  </li>
 
-    return (
-      <ul className='Lista'>
-        {this.props.sounds.map(makeListItem)}
-      </ul>
-    )
-  }
-}
+const SoundList = (props) => (
+  <ul className='Lista'>
+    {props.sounds.map(item =>
+      <ListItem
+        key={item.id}
+        id={item.id}
+        artwork_url={item.artwork_url}
+        userName={item.userName}
+        title={item.title}
+      />)}
+  </ul>
+)
 
 export default SoundList
