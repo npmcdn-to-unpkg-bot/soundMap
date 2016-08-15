a269aa0fba9c77eed1e3eabe2dc430819de4b99a
endziu <koper.andrzej@gmail.com>
2016-08-15 11:52:08 +0200

    removed click function from header

--- src/Header.js
+++ src/Header.js
@@ -2,21 +2,11 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 
 class Header extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {message: 'Dźwiękowa Mapa Wrocławia'}
-    this.headerClick = this.headerClick.bind(this)
-  }
-
-  headerClick(e) {
-    this.setState({message: '-= Welcome everybody to the California City =-'})
-  }
-
   render() {
     return (
-      <div onClick={this.headerClick} className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>{this.state.message}</h2>
+        <h2>Dźwiękowa mapa Wrocławia</h2>
       </div>
     );
   }
