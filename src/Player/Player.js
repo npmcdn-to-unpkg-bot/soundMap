3514195a0962718cb8df522269ccce4dc70b78c0
endziu <koper.andrzej@gmail.com>
2016-08-19 19:58:54 +0200

    add progress bar to the player

--- src/Player/Player.js
+++ src/Player/Player.js
@@ -1,15 +1,21 @@
 import React, { PropTypes, Component } from 'react'
 import { SoundPlayerContainer } from 'react-soundplayer/addons'
-import { PlayButton, Timer } from 'react-soundplayer/components'
+import { PlayButton, Timer, Progress } from 'react-soundplayer/components'
 import './Player.css'
 
 class Player extends Component {
     render() {
-        let { track, currentTime } = this.props;
+        let { track, currentTime, duration } = this.props;
         return (
             <div className="player">
                 <PlayButton className="player-button" {...this.props} />
                 <Timer className="player-timer" duration={track ? track.duration / 1000 : 0} currentTime={currentTime} {...this.props} />
+                <Progress
+                    className="rounded player-progress"
+                    innerClassName="rounded inner"
+                    value={(currentTime / duration) * 100 || 0}
+                    {...this.props}
+                />
             </div>
         )
     }
