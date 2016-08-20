a66dffaa628b777ee25f72c0d1aec3bd22e93d98
endziu <koper.andrzej@gmail.com>
2016-08-20 19:18:19 +0200

    rewrite header to use class sytax

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Player from '../Player/Player'
 import logo from '../assets/logo.svg'
 import './Header.css'
 
-const Header = (props) => {
-  const id = props.soundId
-  const urlBase = 'https://api.soundcloud.com/tracks/'
+class Header extends Component {
+  filterData(e) {
+    e.preventDefault()
+    console.log(e.target.value)
+  }
 
-  return (
-    <div className='App-header clearfix'>
-      <img src={logo} className='App-logo left ml2 mt1 xs-hide' alt='logo' />
-      <h1 className='App-title h2 inline-block sm-hide xs-hide'>Dźwiękowa mapa Wrocławia</h1>
-      <div className='right inline-block m2'>
-        <input className='searchBox' type="text" name="search" placeholder="Search..."></input>
+  render() {
+    const id = this.props.soundId
+    const urlBase = 'https://api.soundcloud.com/tracks/'
+
+    return (
+      <div className='App-header clearfix'>
+        <img src={logo} className='App-logo left ml2 mt1 xs-hide' alt='logo' />
+        <h1 className='App-title h2 inline-block sm-hide xs-hide'>Dźwiękowa mapa Wrocławia</h1>
+        <div className='right inline-block m2'>
+          <input
+            className='searchBox'
+            type='text'
+            name='search'
+            placeholder='Search...'
+            onChange={this.filterData.bind(this)}>
+          </input>
+        </div>
+
+        <Player
+          resolveUrl={urlBase + id}
+          clientId={'5646c69be299b7297f6b389a5b996453'} />
       </div>
-
-      <Player
-        resolveUrl={urlBase + id}
-        clientId={'5646c69be299b7297f6b389a5b996453'} />
-    </div>
-  )
+    )
+  }
 }
+
 export default Header
