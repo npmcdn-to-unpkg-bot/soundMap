29d296a6f824a4ec7282bd8dc9190ebf01fca6aa
endziu <koper.andrzej@gmail.com>
2016-08-21 16:05:00 +0200

    add userInfo component

---
+++ src/UserInfo/UserInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class UserInfo extends Component {
+  render() {
+    return(
+      <div className='Lista-userInfo'>
+        <div id='user'>{this.props.userName}</div>
+        <div id='title' className='truncate'>{this.props.title}</div>
+      </div>
+    )
+  }
+}
+
+export default UserInfo
