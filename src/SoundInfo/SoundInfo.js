3cb4d20d63f46eab5771073a077fec6089202450
endziu <koper.andrzej@gmail.com>
2016-08-25 20:40:05 +0200

    set position of sound-description

--- src/SoundInfo/SoundInfo.js
+++ src/SoundInfo/SoundInfo.js
@@ -4,23 +4,18 @@ import './SoundInfo.css'
 const SoundInfo = (props) => {
   const fullDesc = props.description.split('\n')
   const getOnly = (key) => (item) => item.toLowerCase().search(key) !== -1
-  const loc = fullDesc.filter(getOnly('lokalizacja'))
-  const data = fullDesc.filter(getOnly('data'))
-  const czas = fullDesc.filter(getOnly('czas'))
-  const descr = fullDesc.filter(getOnly('opis'))
-  const gear = fullDesc.filter(getOnly('rekorder'))
+  const loc = fullDesc.filter(getOnly('lokalizacja'))[0]
+  const data = fullDesc.filter(getOnly('data'))[0]
+  const czas = fullDesc.filter(getOnly('czas'))[0]
+  //const descr = fullDesc.filter(getOnly('opis'))[0]
+  const gear = fullDesc.filter(getOnly('rekorder'))[0]
 
   return(
     <div className='SoundInfo'>
-      {loc}
-      <hr />
-      {data}
-      <hr />
-      {czas}
-      <hr />
-      {descr}
-      <hr />
-      {gear}
+      <p>{loc}</p>
+      <p>{data}</p>
+      <p>{czas || 'Czas:'}</p>
+      <p>{gear}</p>
     </div>
   )
 }
