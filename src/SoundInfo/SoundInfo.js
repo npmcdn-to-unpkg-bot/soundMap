630ebc712910e08a0c568f9eecf07fac78bc53cf
endziu <koper.andrzej@gmail.com>
2016-08-25 17:56:54 +0200

    add more sound-info

--- src/SoundInfo/SoundInfo.js
+++ src/SoundInfo/SoundInfo.js
@@ -3,10 +3,22 @@ import './SoundInfo.css'
 
 const SoundInfo = (props) => {
   const fullDesc = props.description
-  const desc = fullDesc.slice(fullDesc.search('Opis') + 5, fullDesc.search('Rekorder'))
+  const loc = fullDesc.slice(fullDesc.search('Lokalizacja') + 12, fullDesc.search('GPS'))
+  const data = fullDesc.slice(fullDesc.search('Data'), fullDesc.search('Czas'))
+  const czas = fullDesc.slice(fullDesc.search('Czas'), fullDesc.search('Długość'))
+  const descr = fullDesc.slice(fullDesc.search('Opis') + 5, fullDesc.search('Rekorder'))
+  const gear = fullDesc.slice(fullDesc.search('Rekorder'), fullDesc.search('Autor'))
   return(
     <div className='SoundInfo'>
-      {desc}
+      {loc}
+      <hr />
+      {data}
+      <hr />
+      {czas}
+      <hr />
+      {descr}
+      <hr />
+      {gear}
     </div>
   )
 }
