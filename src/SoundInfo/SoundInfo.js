13c2c751b9a7495d0f042395b55cbb252e5ca58e
endziu <koper.andrzej@gmail.com>
2016-08-25 18:34:34 +0200

    parse description

--- src/SoundInfo/SoundInfo.js
+++ src/SoundInfo/SoundInfo.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './SoundInfo.css'
 
 const SoundInfo = (props) => {
-  const fullDesc = props.description
-  const loc = fullDesc.slice(fullDesc.search('Lokalizacja') + 12, fullDesc.search('GPS'))
-  const data = fullDesc.slice(fullDesc.search('Data'), fullDesc.search('Czas'))
-  const czas = fullDesc.slice(fullDesc.search('Czas'), fullDesc.search('Długość'))
-  const descr = fullDesc.slice(fullDesc.search('Opis') + 5, fullDesc.search('Rekorder'))
-  const gear = fullDesc.slice(fullDesc.search('Rekorder'), fullDesc.search('Autor'))
+  const fullDesc = props.description.split('\n')
+  const getOnly = (key) => (item) => item.toLowerCase().search(key) !== -1
+  const loc = fullDesc.filter(getOnly('lokalizacja'))
+  const data = fullDesc.filter(getOnly('data'))
+  const czas = fullDesc.filter(getOnly('czas'))
+  const descr = fullDesc.filter(getOnly('opis'))
+  const gear = fullDesc.filter(getOnly('rekorder'))
+
   return(
     <div className='SoundInfo'>
       {loc}
